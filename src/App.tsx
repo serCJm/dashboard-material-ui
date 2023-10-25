879ace6e0505d4be9180c6ee93c8b62931a4bb41
CJ Moro <scjmoro@gmail.com>
2023-10-25 04:53:30 +0000

    Add app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import "./App.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes } from "react-router-dom";
+import Sidebar from "./scenes/global/Sidebar";
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
-	return <div className="app"></div>;
+	const [theme, colorMode] = useMode();
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline></CssBaseline>
+				<div className="app">
+					<Sidebar />
+					<main className="content">
+						<Topbar />
+						<Routes>
+							{/* <Route path="/" element={<Dashboard />} /> */}
+							{/* <Route path="/team" element={<Team />} />
+							<Route path="/contacts" element={<Contacts />} />
+							<Route path="/invoices" element={<Invoices />} />
+							<Route path="/form" element={<Form />} />
+							<Route path="/bar" element={<Bar />} />
+							<Route path="/pie" element={<Pie />} />
+							<Route path="/line" element={<Line />} />
+							<Route path="/faq" element={<FAQ />} />
+							<Route path="/calendar" element={<Calendar />} />
+							<Route path="/geography" element={<Geography />} /> */}
+						</Routes>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
